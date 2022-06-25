d4ae9c88c8393874d288cf0cf1c102e5e644cb5c
bhaveshabcd1234bhavu@gmail.com <bhaveshabcd1234bhavu@gmail.com>
2022-06-25 18:32:45 +0530

    khan

---
+++ main.js
@@ -0,0 +1,115 @@
+console.log('creat bay Bhavesh🤗');
+
+
+var ool =document.getElementById('bt');
+
+function aaaa(){
+  location.reload();
+  
+}
+/*window.*/
+
+
+var nvm = document.getElementById('g');
+
+function d(){
+  
+  nvm.classList.toggle('whit')
+}
+
+function c(){
+  nvm.classList.toggle('hun')
+  ool.classList.toggle('bt')
+}
+
+
+/*&&&&&&&&*/
+
+var a =document.getElementById('op');
+
+var r =document.getElementById('menu_is');
+
+var k =document.getElementById('bt');
+
+
+function opan(){
+  a.classList.toggle('opan');
+  
+  k.classList.toggle('bt');
+  
+}
+
+
+function cll(){
+  r.classList.toggle('pan')
+}
+
+
+/*  and  */
+
+var aa = document.getElementById('hd');
+var bb = document.getElementById('nh');
+var cc = document.getElementById('op');
+var dd = document.getElementById('menu_is');
+var ee = document.getElementById('bt');
+
+function ttt(){
+aa.classList.toggle('t_1')
+  
+bb.classList.toggle('t_1')  
+
+cc.classList.toggle('t_1')
+  
+dd.classList.toggle('t_1') 
+
+
+
+}
+
+
+function bbb(){
+aa.classList.toggle('t_2')
+  
+bb.classList.toggle('t_2')  
+
+cc.classList.toggle('t_2')
+  
+dd.classList.toggle('t_2') 
+  
+}
+
+
+ function hhh(){
+aa.classList.toggle('t_3')
+  
+bb.classList.toggle('t_3')  
+
+cc.classList.toggle('t_3')
+  
+dd.classList.toggle('t_3') 
+
+ee.classList.toggle('t_3') 
+}
+
+
+function t_4(){ 
+  "   "
+}
+
+
+function hh(){
+  /*var hud = document.getElementById('g').nodeValue;
+  */
+var hud = location
+
+   hud.reload();
+}
+
+/*
+
+
+bhaveshabcd1234bhavu @gmail.com
+
+9106885928 h
+
+*/
